355784adf52f1cc9267fb0f74aaa3242cb8e7948
jaky1206 <jaky1206@gmail.com>
2016-07-20 18:59:53 +0600

    SequenceOfNumbersUsingGenericQueueClass final commit

--- IntroToCSharp/Start.cs
+++ IntroToCSharp/Start.cs
@@ -271,7 +271,21 @@ namespace IntroToCSharp
             Queue<int> queue = new Queue<int>();
             queue.Enqueue(n);
             int index = 0;
-            Console.WriteLine("S =");
+            Console.Write("S =");
+            while (queue.Count > 0)
+            {
+                index++;
+                int current = queue.Dequeue();
+                Console.Write(" " + current);
+                if (current == p)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("index = " + index);
+                    return;
+                }
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current);
+            }
 
             ClearAfterKeyPress();
         }
