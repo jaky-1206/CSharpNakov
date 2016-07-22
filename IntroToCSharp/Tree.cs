83c9d9f22cc192139e9b92346cb8380c51f242ce
jaky1206 <jaky1206@gmail.com>
2016-07-22 18:26:09 +0600

    dynamic tree representation

---
+++ IntroToCSharp/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroToCSharp
+{
+    /// <summary>Represents a tree data structure</summary>
+    /// <typeparam name="T">the type of the values in the
+    /// tree</typeparam>
+    public class Tree<T>
+    {
+        // The root of the tree
+        private TreeNode<T> root;
+
+        /// <summary>Constructs the tree</summary>
+        /// <param name="value">the value of the node</param>
+        public Tree(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Can not insert null value!");
+            }
+            this.root = new TreeNode<T>(value);
+        }
+    }
+}
