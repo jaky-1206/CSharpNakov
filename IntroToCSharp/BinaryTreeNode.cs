11abbc9176478a2dff1e24285b8f704b7f062712
jaky1206 <jaky1206@gmail.com>
2016-07-24 17:57:26 +0600

    BinaryTreeNode initial commit

---
+++ IntroToCSharp/BinaryTreeNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharp
+{
+    internal class BinaryTreeNode<T> : IComparable<BinaryTreeNode<T>> where T : IComparable<T>
+    {
+        // Contains the value of the node
+        internal T value;
+        // Contains the parent of the node
+        internal BinaryTreeNode<T> parent;
+        // Contains the left child of the node
+        internal BinaryTreeNode<T> leftChild;
+        // Contains the right child of the node
+        internal BinaryTreeNode<T> rightChild;
+
+        /// <summary>Constructs the tree node</summary>
+        /// <param name="value">The value of the tree node</param>
+        public BinaryTreeNode(T value)
+        {
+            if (value == null)
+            {
+                // Null values cannot be compared -> do not allow them
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+            this.value = value;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            BinaryTreeNode<T> other = (BinaryTreeNode<T>)obj;
+            return this.CompareTo(other) == 0;
+        }
+
+        public int CompareTo(BinaryTreeNode<T> other)
+        {
+            return this.value.CompareTo(other.value);
+        }
+    }
+}
